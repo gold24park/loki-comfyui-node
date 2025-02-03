247bc040702b9cc5b6826f1d9509600c1030a84f
lou <gold24park@gmail.com>
2025-02-03 18:25:18 +0900

    OverlayText

--- __init__.py
+++ __init__.py
@@ -1,14 +1,17 @@
 from .luminance import ImageLuminance
 from .dominant_color import DominantColor
+from .overlay_text import OverlayText
 
 NODE_CLASS_MAPPINGS = {
     "ImageLuminance": ImageLuminance,
-    "DominantColor": DominantColor
+    "DominantColor": DominantColor,
+    "OverlayText": OverlayText
 }
 
 NODE_DISPLAY_NAMES_MAPPINGS = {
     "ImageLuminance": "Get Image Luminance",
-    "DominantColor": "Get Dominant Color"
+    "DominantColor": "Get Dominant Color",
+    "OverlayText": "Overlay Text"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
---
+++ overlay_text.py
@@ -0,0 +1,75 @@
+import numpy as np
+import torch
+import os
+import platform
+from .functions_graphics import *
+from .config import color_mapping, COLORS
+from PIL import Image, ImageDraw, ImageFont
+
+ALIGN_OPTIONS = ["center", "top", "bottom"]
+ROTATE_OPTIONS = ["text center", "image center"]
+JUSTIFY_OPTIONS = ["center", "left", "right"]
+PERSPECTIVE_OPTIONS = ["top", "bottom", "left", "right"]
+
+class OverlayText:
+
+    @classmethod
+    def INPUT_TYPES(s):
+
+        font_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "fonts")
+        file_list = [f for f in os.listdir(font_dir) if os.path.isfile(os.path.join(font_dir, f)) and f.lower().endswith(".ttf")]
+
+        return {"required": {
+                "image": ("IMAGE",),
+                "text": ("STRING", {"multiline": True, "default": "text"}),
+                "font_name": (file_list,),
+                "font_size": ("INT", {"default": 50, "min": 1, "max": 1024}),
+                "font_color": (COLORS,),
+                "align": (ALIGN_OPTIONS,),
+                "justify": (JUSTIFY_OPTIONS,),
+                "margins": ("INT", {"default": 0, "min": -1024, "max": 1024}),
+                "line_spacing": ("INT", {"default": 0, "min": -1024, "max": 1024}),
+                "position_x": ("INT", {"default": 0, "min": -4096, "max": 4096}),
+                "position_y": ("INT", {"default": 0, "min": -4096, "max": 4096}),
+                "rotation_angle": ("FLOAT", {"default": 0.0, "min": -360.0, "max": 360.0, "step": 0.1}),
+                "rotation_options": (ROTATE_OPTIONS,),
+                },
+                "optional": {"font_color_hex": ("STRING", {"multiline": False, "default": "#000000"})
+                }
+    }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "overlay_text"
+    CATEGORY = "LokiComfyUINode/Text"
+
+    def overlay_text(self, image, text, font_name, font_size, font_color,
+                     margins, line_spacing,
+                     position_x, position_y,
+                     align, justify,
+                     rotation_angle, rotation_options,
+                     font_color_hex='#000000'):
+
+        # Get RGB values for the text color
+        text_color = get_color_values(font_color, font_color_hex, color_mapping)
+
+        # Convert tensor images
+        image_3d = image[0, :, :, :]
+
+        # Create PIL images for the text and background layers and text mask
+        back_image = tensor2pil(image_3d)
+        text_image = Image.new('RGB', back_image.size, text_color)
+        text_mask = Image.new('L', back_image.size)
+
+        # Draw the text on the text mask
+        rotated_text_mask = draw_masked_text(text_mask, text, font_name, font_size,
+                                             margins, line_spacing,
+                                             position_x, position_y,
+                                             align, justify,
+                                             rotation_angle, rotation_options)
+
+        # Composite the text image onto the background image using the rotated text mask
+        image_out = Image.composite(text_image, back_image, rotated_text_mask)
+
+        # Convert the PIL image back to a torch tensor
+        return (pil2tensor(image_out),)
