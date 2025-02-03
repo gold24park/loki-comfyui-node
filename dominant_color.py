8cdffc4dcc77337b4d06098e5cfbb301ac5241d4
lou <gold24park@gmail.com>
2025-02-03 16:36:51 +0900

    Dominant Color : add max brightness

--- dominant_color.py
+++ dominant_color.py
@@ -1,13 +1,17 @@
 import numpy as np
 from PIL import Image
 from sklearn.cluster import KMeans
+import colorsys
+
 
 class DominantColor:
     @classmethod
     def INPUT_TYPES(cls):
         inputs = {
             "required": {
-                "image": ("IMAGE",)
+                "image": ("IMAGE",),
+                "num_colors": ("INT", {"default": 1, "min": 1, "max": 10}),
+                "max_brightness": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0}),
             }
         }
         return inputs
@@ -18,7 +22,15 @@ class DominantColor:
     OUTPUT_NODE = True
     CATEGORY = "LokiComfyUINode/Image Analysis"
     
-    def get_dominant_color(self, image, num_colors=1):
+    def is_dark_color(self, rgb_color, max_brightness):
+        # Convert RGB to HSV
+        r, g, b = rgb_color[0] / 255.0, rgb_color[1] / 255.0, rgb_color[2] / 255.0
+        h, s, v = colorsys.rgb_to_hsv(r, g, b)
+        
+        # Check if the color's value (brightness) is below the threshold
+        return v <= max_brightness
+    
+    def get_dominant_color(self, image, num_colors=1, max_brightness=0.5):
         # Convert the tensor to PIL Image
         if len(image.shape) == 4:
             image = image[0]
@@ -28,15 +40,28 @@ class DominantColor:
         h, w, _ = image_array.shape
         image_array_reshaped = image_array.reshape((h * w, 3))
         
-        # Use KMeans to find the clusters of colors
-        kmeans = KMeans(n_clusters=num_colors, random_state=42)
+        # Use KMeans to find more clusters than requested to ensure we have enough dark colors
+        kmeans = KMeans(n_clusters=min(num_colors * 3, h * w), random_state=42)
         kmeans.fit(image_array_reshaped)
         
-        # Get the dominant colors
+        # Get the dominant colors and filter dark ones
         colors = kmeans.cluster_centers_
+        dark_colors = [color for color in colors if self.is_dark_color(color, max_brightness)]
         
+        # Sort dark colors by frequency (using cluster labels)
+        color_frequencies = np.bincount(kmeans.labels_)
+        dark_color_indices = [i for i, color in enumerate(colors) if self.is_dark_color(color, max_brightness)]
+        dark_colors_with_freq = [(colors[i], color_frequencies[i]) for i in dark_color_indices]
+        dark_colors_with_freq.sort(key=lambda x: x[1], reverse=True)
+        
+        # Take the requested number of dark colors
+        final_colors = [color for color, _ in dark_colors_with_freq[:num_colors]]
+        
+        if not final_colors:
+            return ("#000000",)  # Return black if no dark colors found
+            
         # Convert to hex color codes
         hex_colors = ['#{:02x}{:02x}{:02x}'.format(int(c[0]), int(c[1]), int(c[2])) 
-                     for c in colors]
+                     for c in final_colors]
         
         return (hex_colors[0] if num_colors == 1 else ', '.join(hex_colors),)
